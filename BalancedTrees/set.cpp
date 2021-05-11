9af7b962facdae5d47a9abaa9549bc7fc65b4d96
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-11 18:52:16 +0900

    insert 함수 추가. fix_excess를 어떻게 구현해야하나

--- BalancedTrees/set.cpp
+++ BalancedTrees/set.cpp
@@ -1,5 +1,7 @@
 #include "set.h"
 
+#include <algorithm>
+
 namespace main_savitch_11
 {
     template <typename Item>
@@ -48,5 +50,82 @@ namespace main_savitch_11
         }
         
         return subset[nSubSetIdx]->count(target);
+    }
+
+    template <typename Item>
+    bool set<Item>::insert(const Item& entry)
+    {
+        if(!loose_insert(entry))
+        {
+            return false;
+        }
+        
+        // 최상위 root에서 fix_excess 처리
+        if(data_count > MAXIMUM)
+        {
+            
+        }
+
+        return true;
+    }
+
+    // Precondition: The entire B-tree is valid.
+    // Postcondition: If entry was already in the set, then the set is unchanged and the return value is false.
+    // Otherwise, the entry has been added to the set, the return value is true, and the entire B-tree is still
+    // valid EXCEPT that the number of entries in the root of this set might be one more than the allowed maximum.
+    template <typename Item>
+    bool set<Item>::loose_insert(const Item& entry)
+    {
+        int nSubSetIdx = -1;
+        for(int i = 0; i < data_count; i++)
+        {
+            if(data[i] > entry)
+            {
+                nSubSetIdx = i;
+                break;
+            }
+        }
+
+        if(nSubSetIdx == -1)
+        {
+            nSubSetIdx = data_count;
+        }
+
+        // root에 추가하려는 entry가 이미 존재하는 경우
+        if( (nSubSetIdx < data_count) && !(data[nSubSetIdx] > entry) )
+        {
+            return false;
+        }
+        else if(is_leaf())
+        {
+            data[nSubSetIdx] = entry;
+            data_count++;
+            std::sort(data, data + data_count);
+            return true;
+        }
+        else
+        {
+            bool nRet = subset[nSubSetIdx]->loose_insert(entry);
+            if(nRet)
+            {
+                if(subset[nSubSetIdx]->data_count > MAXIMUM)
+                    fix_excess(nSubSetIdx);
+            }
+        }
+
+        return false;
+    }
+
+    // Precondition: (i < child_count) and the entire B-tree is valid EXCEPT that subset[i] has MAXIMUM + 1 entries.
+    // Postcondition: The tree has been rearranged so that the entire B-tree is valid EXCEPT that the number of entries
+    // in the root of this set might be one more than the allowed maximum.
+    template<typename Item>
+    void set<Item>::fix_excess(std::size_t i)
+    {
+        int midIdx = subset[i]->data_count / 2;
+
+        // 가운데 entity를 parent로 올리고
+
+        // subset 2개로 분할하기
     }
 }
