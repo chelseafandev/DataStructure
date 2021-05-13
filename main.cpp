3fcf4c8248813d3daf727c17ecdcef58f91f393b
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-13 15:52:48 +0900

    테스트용 main문 수정

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 #include "BalancedTrees/set.h"
+#include "BinaryTreeNode/bintree.h"
 
 int main()
 {
     main_savitch_11::set<int> mySet;
+    main_savitch_10::binary_tree_node<int> myBTNode;
     return 0;
 }
