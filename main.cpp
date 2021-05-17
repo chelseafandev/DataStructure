c3d6fee0b649631433c352ad23353faba0795992
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-17 18:35:47 +0900

    템플릿 클래스 정의할때는 헤더에 다 넣자.. 이것땜에 개고생

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-#include "BalancedTrees/set.h"
 #include "BinaryTreeNode/bintree.h"
 
 int main()
 {
-    main_savitch_11::set<int> mySet;
-    main_savitch_10::binary_tree_node<int> myBTNode;
+    main_savitch_10::binary_tree_node<int> rootNode(100);
+    main_savitch_10::binary_tree_node<int> leftNode(200);
+    main_savitch_10::binary_tree_node<int> rightNode(300);
+    main_savitch_10::binary_tree_node<int> lastNode(400);
+
+    rootNode.set_left(&leftNode);
+    rootNode.set_right(&rightNode);
+    leftNode.set_left(&lastNode);
+
+    main_savitch_10::print<int, int>(&rootNode, 0);
     return 0;
 }
