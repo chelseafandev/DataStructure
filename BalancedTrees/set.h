a8e6818677e20586266a0434a71a084c5f831898
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-26 17:49:23 +0900

    set 클래스의 insert 과정 주석으로 설명

--- BalancedTrees/set.h
+++ BalancedTrees/set.h
@@ -109,22 +109,29 @@ namespace main_savitch_11
 
         bool insert(const Item &entry)
         {
+            // loose_insert 함수를 통해서 인자로 넘어오는 entry가 추가될 leaf 노드를 찾음.
+            // 이 leaf 노드는 예외적으로 maximum보다 하나 더 많은 entry 개수를 가질 수 있으며,
+            // 이 예외는 fix_excess라는 함수를 통해 처리하게 됨.
             if (!loose_insert(entry))
             {
                 std::cout << "loose_insert failed!" << std::endl;
                 return false;
             }
 
-            //std::cout << "data_count = " << data_count << std::endl;
-            //std::cout << "MAXIMUM = " << MAXIMUM << std::endl;
-            // 최상위 root에서 fix_excess 처리
+            // loose_insert를 재귀적으로 호출하여 최상위 root에 까지 도달한 다음에도
+            // root 노드의 entry개수가 maximum을 초과할 수 있음. 아래는 이를 처리하기 위한 로직임.
             if (data_count > MAXIMUM)
             {
+                // 기존의 root 노드를 복사
                 set<Item> *tmp = new set<Item>();
                 *tmp = *this;
+                
+                // root 노드 초기화 이후, 복사해둔 노드를 subset으로 추가 
                 clear();                
                 child_count = 1;
                 subset[0] = tmp;
+
+                // subset으로 추가된 기존의 root 노드에 대한 fix_excess 함수 처리
                 fix_excess(0);
             }
             return true;
@@ -225,13 +232,15 @@ namespace main_savitch_11
         // 헬퍼 멤버 함수
         bool is_leaf() const { return child_count == 0; }
 
-        // Precondition: The entire B-tree is valid.
+        // Precondition: 전체 B-tree는 유효하다.
+        // Postcondition: 만약 set 안에 이미 존재하는 값이 entry라면, set에 변동은 없으며 false를 반환한다.
         // Postcondition: If entry was already in the set, then the set is unchanged and the return value is false.
-        // Otherwise, the entry has been added to the set, the return value is true, and the entire B-tree is still
-        // valid EXCEPT that the number of entries in the root of this set might be one more than the allowed maximum.
+        // 그렇지 않다면, entry값은 set에 추가되고 true를 반환한다. 그리고 현재 set의 root의 entry 개수가 maximum값 보다 
+        // 1이 더 큰 경우를 제외한 전체 B-tree는 여전히 유효하다.
         bool loose_insert(const Item &entry)
         {
-            // entry의 인덱스 값 결정
+            // 새롭게 추가 될 entry의 값을 통해 어떠한 subset에 포함이 되어야하는지 결정(subset의 인덱스 값)
+            // entry의 값을 현재 노드의 data 값들과 비교해 나감
             int nSubSetIdx = -1;
             for (int i = 0; i < data_count; i++)
             {
@@ -241,13 +250,12 @@ namespace main_savitch_11
                     break;
                 }
             }
-
+            
             if (nSubSetIdx == -1)
             {
                 nSubSetIdx = data_count;
             }
-            ///
-
+            
             if ((nSubSetIdx < data_count) && !(data[nSubSetIdx] > entry))
             {
                 // root에 추가하려는 entry가 이미 존재하는 경우
@@ -255,7 +263,10 @@ namespace main_savitch_11
             }
             else if (is_leaf())
             {
-                // leaf 노드인 경우 위에서 구한 인덱스 값을 이용하여 entry값을 data 배열에 추가
+                // leaf 노드인 경우 위에서 구한 subset의 인덱스 값을 이용하여 entry값을 data 배열에 추가.
+                // entry를 추가하더라도 data 배열은 오름차순으로 정렬이 유지되어야함.
+
+                // data 배열이 empty인 경우
                 if (data_count == 0)
                 {
                     data[0] = entry;
@@ -263,6 +274,7 @@ namespace main_savitch_11
                     return true;
                 }
 
+                // 정렬 수행
                 bool isFound = false;
                 Item prev;
                 data_count++;
@@ -294,10 +306,11 @@ namespace main_savitch_11
             }
             else
             {
-                // leaf 노드가 아닌 경우
+                // leaf 노드가 아닌 경우는 loose_insert 함수 재귀 호출
                 bool nRet = subset[nSubSetIdx]->loose_insert(entry);
                 if (nRet)
                 {
+                    // loose_insert를 통해 leaf 노드에 값이 추가됐다면, leaf 노드의 data_count가 maximum을 초과하는 이슈를 해결해야함.
                     if (subset[nSubSetIdx]->data_count > MAXIMUM)
                         fix_excess(nSubSetIdx);
 
@@ -316,18 +329,19 @@ namespace main_savitch_11
         {
         }
 
-        // Precondition: (i < child_count) and the entire B-tree is valid EXCEPT that subset[i] has MAXIMUM + 1 entries.
-        // Postcondition: The tree has been rearranged so that the entire B-tree is valid EXCEPT that the number of entries
-        // in the root of this set might be one more than the allowed maximum.
+        // Precondition: i는 child_count보다는 작고, subset[i]의 data count가 MAXIMUM + 1인 예외를 제외하고는 전체 B-tree가 유효하다.
+        // Postcondition: tree는 재정렬되며 이를 통해 이 set의 root노드가 가지는 entry의 개수가 허용하는 maximum 개수를 초과하는 경우를 
+        // 제외하면 전체 B-tree는 유효하다.
         void fix_excess(std::size_t i)
         {
+            // maximum 개수를 초과한 data 배열에서 위쪽 노드로 올려보내는 값은 중간에 위치한 값임.
             int midIdx = subset[i]->data_count / 2;
 
-            // subset의 가운데 entity를 data에 저장하고
+            // maximum 개수를 초과한 subset data 배열의 중간 값을 현재 노드의 data 배열에 저장하고
             data[i] = subset[i]->data[midIdx];
             data_count++;
 
-            // right
+            // subset 쪼개기(오른쪽) - 새로운 set을 만들어서 data 및 subset 붙이기
             set<Item> *added = new set<Item>();
             int newIdx = 0;
             for (int idx = midIdx + 1; idx < subset[i]->data_count; idx++)
@@ -339,17 +353,17 @@ namespace main_savitch_11
             child_count++;;
             subset[child_count - 1] = added;
 
-            // left
+            // subset 쪼개기(왼쪽) - 기존 subset 활용하기(count값만 변경)
             subset[i]->data_count = midIdx;
 
-            // subset of subset
+            // 분할된 subset의 subset 붙이기
             if(subset[i]->child_count != 0)
             {
-                //left
+                // 분할된 왼쪽 subset에 subset 붙이기
                 int leftSosCnt = subset[i]->data_count + 1;
                 subset[i]->child_count = leftSosCnt;
 
-                // right
+                // 분할된 오른쪽 subset에 subset 붙이기
                 int rightSosCnt = subset[child_count - 1]->data_count + 1;
                 for (int j = 0, idx = leftSosCnt; idx < rightSosCnt + leftSosCnt; j++, idx++)
                 {
